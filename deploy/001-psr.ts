ba254e6bc82561eea723d985c9ae581ac4f9bb55
Ainomo <167073179+ainomoai@users.noreply.github.com>
2024-05-23 01:00:50 -0700

    Update 001-psr.ts

--- deploy/001-psr.ts
+++ deploy/001-psr.ts
@@ -1,4 +1,4 @@
-4444444444import hre, { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
