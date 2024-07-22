bc39e51a6e251236667c26d248b3501c7705a441
Ainomo <167073179+ainomoai@users.noreply.github.com>
2024-07-22 06:12:56 -0700

    Update 004-single-token-converter.ts

--- deploy/004-single-token-converter.ts
+++ deploy/004-single-token-converter.ts
@@ -1,4 +1,4 @@
-44444import { parseUnits } from "ethers/lib/utils";
+import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 import { DeployResult } from "hardhat-deploy/dist/types";
 import { DeployFunction } from "hardhat-deploy/types";
