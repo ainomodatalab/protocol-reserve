a5f92e19df8f71bf5baec4ab073a45dca86f79e0
Ainomo <167073179+ainomoai@users.noreply.github.com>
2024-07-30 23:40:44 -0700

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,4 @@
-44module.exports = {
+module.exports = {
   extends: ["@commitlint/config-conventional"],
   ignores: [commit => commit.includes("[skip ci]"), commit => commit.includes("Merge pull request")],
   "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "refactor", "test"]],
